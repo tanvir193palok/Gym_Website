8bdfaafabe84101c6b2cc45946515caec8f611f0
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-01 05:07:16 +0600

    Faq and accordion component created

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,15 +7,40 @@ import { faq } from "../data";
 import Accordion from "../components/Accordion";
 
 const Faq = () => {
+  
   //Destructuring faq data
   const { icon, title, accordions } = faq;
   return (
     <section className="section pt-[480px] lg:pt-[280px]">
-      <div>
+      <div
+        className="max-w-[768px] mx-auto lg:bg-faq 
+      bg-no-repeat bg-custom bg-center lg:h-[1160px] lg:pt-6"
+      >
+
         {/* Title Section */}
-        <div>
+        <div
+          className="section-title-group justify-start lg:justify-center
+         -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0"
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-delay="200"
+        >
           <img className="lg:hidden" src={icon} alt="" />
-          <h2 className="h2">{title}</h2>
+          <h2 className="h2 section-title lg:mt-[100px]">
+            {title} <span className="text-primary-200">.</span>
+          </h2>
+        </div>
+
+        {/* accordion List */}
+        <div
+          className="flex flex-col gap-y-4"
+          data-aos="fade-up"
+          data-aos-offset="300"
+          data-aos-delay="200"
+        >
+          {accordions.map((accordion, idx) => {
+            return <Accordion accordion={accordion} key={idx} />;
+          })}
         </div>
       </div>
     </section>

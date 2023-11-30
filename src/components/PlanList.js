a619d682541192733c9249a4b314f205802d29d0
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-11-30 22:07:07 +0600

    PlanList and Pricing component updated

--- src/components/PlanList.js
+++ src/components/PlanList.js
@@ -20,44 +20,71 @@ const PlanList = ({ plans }) => {
             className="w-full md:max-w-[620px] lg:max-w-[405px] rounded-sm
               px-4 lg:min-h-[550px]"
             key={currentIndex}
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay={delay}
           >
             <div
               className={`${
                 currentIndex === index
                   ? "bg-neutral-500 text-white"
                   : "bg-neutral-400/10 text-neutral-500"
-              }`}
+              } flex justify-center items-center py-[40px] px-[30px] lg:min-h-[550px] transition duration-100`}
             >
-              {/*name and price wrapper */}
-              <div>
-                {/*name */}
-                <div>{name}</div>
-                {/*price */}
+              <div className="flex flex-row lg:flex-col gap-x-8 gap-y-8 lg:gap-x-0 items-center">
+                {/*name and price wrapper */}
                 <div>
-                  <div>
-                    <span>{price}</span>
-                    <span>$</span>
+                  {/*name */}
+                  <div
+                    className={`${
+                      currentIndex === index
+                        ? "bg-white text-neutral-500"
+                        : "bg-neutral-500 text-white"
+                    } h-[26px] font-primary text-sm font-semibold
+                     min-w-min mx-auto px-[14px] flex items-center justify-center mb-8`}
+                  >
+                    {name}
                   </div>
-                  <span>/month</span>
+                  {/*price */}
+                  <div
+                    className="text-[40px] lg:text-[50px] font-primary font-extrabold
+                   text-center flex flex-col items-center justify-center"
+                  >
+                    <div className="leading-none">
+                      <span className="tracking-[0.1px]">{price}</span>
+                      <span className="text-[30px] font-extrabold">$</span>
+                    </div>
+                    <span className="text-sm font-medium">/month</span>
+                  </div>
                 </div>
-              </div>
+                {/*List & btn wrapper */}
+                <div>
+                  {/*List */}
+                  <ul className="flex flex-col gap-y-4 mb-8">
+                    {list.map((item, idx) => {
+                      return (
+                        <li
+                          className="flex items-center gap-x-[10px]"
+                          key={idx}
+                        >
+                          <BsCheckCircleFill className="text-lg " />
+                          <div>{item.name}</div>
+                        </li>
+                      );
+                    })}
+                  </ul>
 
-              {/*List & btn wrapper */}
-              <div>
-                {/*List */}
-                <ul className="flex flex-col gap-y-4 mb-8">
-                  {list.map((item, idx) => {
-                    return (
-                      <li className="flex items-center gap-x-[10px]" key={idx}>
-                        <BsCheckCircleFill className="text-lg " />
-                        <div>{item.name}</div>
-                      </li>
-                    );
-                  })}
-                </ul>
-
-                {/* btn */}
-                <button></button>
+                  {/* btn */}
+                  <button
+                    className={`${
+                      currentIndex === index
+                        ? "bg-white text-neutral-500"
+                        : "border border-neutral-500"
+                    } btn btn-lg rounded-[1px] lg:mx-auto`}
+                  >
+                    Join now
+                  </button>
+                </div>
               </div>
             </div>
           </div>
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -13,9 +13,16 @@ const Pricing = () => {
   return (
     <section className="section">
       {/*Title section */}
-      <div className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0">
-        <img src={icon} alt='' />
-        <h2 className="h2">{title} <span className="text-primary-200">.</span></h2>
+      <div
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="" />
+        <h2 className="h2">
+          {title} <span className="text-primary-200">.</span>
+        </h2>
       </div>
 
       {/*Plan list section */}

b070509f26340946694a1eea779c88dabd195de2
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2023-12-01 04:21:53 +0600

    Community and CommunitySLider component updated

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+
+//import data
+import { faq } from "../data";
+
+//import components
+import Accordion from "../components/Accordion";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  //Destructuring faq data
+  const { icon, title, accordions } = faq;
+  return (
+    <section className="section pt-[480px] lg:pt-[280px]">
+      <div>
+        {/* Title Section */}
+        <div>
+          <img className="lg:hidden" src={icon} alt="" />
+          <h2 className="h2">{title}</h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Faq;
